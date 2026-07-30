905cb4cbb07820b074247f034e3963490c6a6511
alcc2810 <collantes.adrian@outlook.com>
2026-07-29 22:32:09 -0500

    backend: define device physical status model

--- backend/device-service/internal/domain/device.go
+++ backend/device-service/internal/domain/device.go
@@ -1,5 +1,18 @@
 package domain
 
+type DevicePhysicalStatus string
+
+const (
+	DevicePhysicalStatusOperational DevicePhysicalStatus = "OPT"
+	DevicePhysicalStatusMaintenance DevicePhysicalStatus = "MNT"
+	DevicePhysicalStatusDamaged     DevicePhysicalStatus = "DMG"
+	DevicePhysicalStatusRetired     DevicePhysicalStatus = "RTD"
+)
+
 type Device struct {
-	IMEI string
+	IMEI            string
+	Model           string
+	FirmwareVersion string
+	BatteryLevel    uint8
+	PhysicalStatus  DevicePhysicalStatus
 }
